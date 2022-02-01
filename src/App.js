4b5bce28dc3ff885501acbe637a2cdce468cdb70
NicolasSanchezAlvarez <nsanchezalvarez483@gmail.com>
2022-01-31 22:12:39 -0300

    agrego itemCount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import NavBar from './componets/NavBar/NavBar':
+import NavBar from './componets/NavBar/NavBar';
 import ItemListContainer from './componets/ItemListContainer/ItemListContainer';
+import ItemCount from './componets/Itemcount/ItemCount';
 
 function App() {
   return (
+    <>
     <NavBar/>
     <ItemListContainer/>
+    <ItemCount/>
+    </>
   );
 }
 
---
+++ src/componets/ItemCount/ItemCount.js
@@ -0,0 +1,27 @@
+import React,{ useState} from 'react';
+
+export default function ItemCount({stock}){
+    const[itemCount, setItemCount] = useState(0)
+
+    const updateItem = () => {
+        itemCount < stock && setItemCount(itemCount + 1)
+    }
+
+    const removeItem = () => {
+        itemCount > 0 && setItemCount(itemCount - 1)
+    }
+
+    return(
+        //Montaje
+        <div>
+            
+            <h1>Titulo item</h1>
+            <button onClick={updateItem}>+</button>
+            <button onClick={removeItem}>-</button>
+            <br/>
+            <button>Agregar al carrito</button>
+           
+        </div>
+    )
+    
+}
